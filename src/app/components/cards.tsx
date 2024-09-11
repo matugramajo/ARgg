6a35cb1ca9080418610fdcdf4ad04a498772128b
Matilde Gramajo <matildegramajo@hotmail.com>
2024-09-11 12:26:35 -0300

    agregue un carousel c:

---
+++ src/app/components/cards.tsx
@@ -0,0 +1,187 @@
+import React from "react";
+
+const Cards: React.FC = () => {
+  return (
+    <div className="flex flex-row justify-between mx-24 mt-7 text-lg">
+      <div className="basis-2/5 pr-40">
+        <div className="pb-3">
+          <a>Proximos partidos</a>
+        </div>
+        <div className="flex flex-col bg-[#232240] rounded-lg">
+          <div className="border-b border-b-[#9199E1] py-2">
+            <div className="flex justify-between px-2 text-base text-[#a1a1a1]">
+              <div>05/09/2024, 11:30hs</div>
+              <div>Counter-Strike 2</div>
+            </div>
+            <div className="grid grid-cols-3 justify-between py-4">
+              <div className="justify-self-end">
+                <img src="/Logo Shinden.png" className="h-16" alt="Logo ARgg" />
+              </div>
+              <div className="justify-self-center content-center">VS</div>
+              <div className="justify-self-start">
+                <img src="/Logo Velox.png" className="h-16" alt="Logo ARgg" />
+              </div>
+            </div>
+            <div className="px-2 text-base text-[#a1a1a1]">BO3</div>
+          </div>
+          <div className="border-b border-b-[#9199E1] py-2">
+            <div className="flex justify-between px-2 text-base text-[#a1a1a1]">
+              <div>05/09/2024, 11:30hs</div>
+              <div>Counter-Strike 2</div>
+            </div>
+            <div className="grid grid-cols-3 justify-between py-4">
+              <div className="justify-self-end">
+                <img src="/Logo Shinden.png" className="h-16" alt="Logo ARgg" />
+              </div>
+              <div className="justify-self-center content-center">VS</div>
+              <div className="justify-self-start">
+                <img src="/Logo Velox.png" className="h-16" alt="Logo ARgg" />
+              </div>
+            </div>
+            <div className="px-2 text-base text-[#a1a1a1]">BO3</div>
+          </div>
+          <div className="border-b border-b-[#9199E1] py-2">
+            <div className="flex justify-between px-2 text-base text-[#a1a1a1]">
+              <div>05/09/2024, 11:30hs</div>
+              <div>Counter-Strike 2</div>
+            </div>
+            <div className="grid grid-cols-3 justify-between py-4">
+              <div className="justify-self-end">
+                <img src="/Logo Shinden.png" className="h-16" alt="Logo ARgg" />
+              </div>
+              <div className="justify-self-center content-center">VS</div>
+              <div className="justify-self-start">
+                <img src="/Logo Velox.png" className="h-16" alt="Logo ARgg" />
+              </div>
+            </div>
+            <div className="px-2 text-base text-[#a1a1a1]">BO3</div>
+          </div>
+          <div className="py-2">
+            <div className="flex justify-between px-2 text-base text-[#a1a1a1]">
+              <div>05/09/2024, 11:30hs</div>
+              <div>Counter-Strike 2</div>
+            </div>
+            <div className="grid grid-cols-3 justify-between py-4">
+              <div className="justify-self-end">
+                <img src="/Logo Shinden.png" className="h-16" alt="Logo ARgg" />
+              </div>
+              <div className="justify-self-center content-center">VS</div>
+              <div className="justify-self-start">
+                <img src="/Logo Velox.png" className="h-16" alt="Logo ARgg" />
+              </div>
+            </div>
+            <div className="px-2 text-base text-[#a1a1a1]">BO3</div>
+          </div>
+        </div>
+      </div>
+      <div className="basis-2/5 pr-40">
+        <div className="pb-3">
+          <a>Ultimas discusiones</a>
+        </div>
+        <div className="flex flex-col bg-[#232240] rounded-lg">
+          <div className="border-b border-b-[#9199E1]">
+            <div className="flex flex-col pl-2 py-2">
+              <div className="text-base text-[#a1a1a1] pb-1">
+                10 de septiembre 2024
+              </div>
+              <div className="text-base text-[#a1a1a1] pb-1">
+                Counter-Strike 2
+              </div>
+              <div className="text-xl py-1">
+                Como desactivar la toxicidad del CS2
+              </div>
+              <div className="text-base text-[#3d5bdf] underline pt-1">
+                <a href="#">Leer mas</a>
+              </div>
+            </div>
+          </div>
+          <div className="border-b border-b-[#9199E1]">
+            <div className="flex flex-col pl-2 py-2">
+              <div className="text-base text-[#a1a1a1] pb-1">
+                10 de septiembre 2024
+              </div>
+              <div className="text-base text-[#a1a1a1] pb-1">
+                Counter-Strike 2
+              </div>
+              <div className="text-xl py-1">
+                Como desactivar la toxicidad del CS2
+              </div>
+              <div className="text-base text-[#3d5bdf] underline pt-1">
+                <a href="#">Leer mas</a>
+              </div>
+            </div>
+          </div>
+          <div className="border-b border-b-[#9199E1]">
+            <div className="flex flex-col pl-2 py-2">
+              <div className="text-base text-[#a1a1a1] pb-1">
+                10 de septiembre 2024
+              </div>
+              <div className="text-base text-[#a1a1a1] pb-1">
+                Counter-Strike 2
+              </div>
+              <div className="text-xl py-1">
+                Como desactivar la toxicidad del CS2
+              </div>
+              <div className="text-base text-[#3d5bdf] underline pt-1">
+                <a href="#">Leer mas</a>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-col pl-2 py-2">
+              <div className="text-base text-[#a1a1a1] pb-1">
+                10 de septiembre 2024
+              </div>
+              <div className="text-base text-[#a1a1a1] pb-1">
+                Counter-Strike 2
+              </div>
+              <div className="text-xl py-1">
+                Como desactivar la toxicidad del CS2
+              </div>
+              <div className="text-base text-[#3d5bdf] underline pt-1">
+                <a href="#">Leer mas</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="basis-1/5">
+        <div className="pb-3">
+          <a>Equipos mas visitados</a>
+        </div>
+        <div className="flex flex-col bg-[#232240] rounded-lg">
+          <div className="grid grid-flow-col border-b border-b-[#9199E1] py-2">
+            <img
+              src="/Logo Shinden.png"
+              className="h-16 justify-self-center"
+              alt="Logo ARgg"
+            />
+          </div>
+          <div className="grid grid-flow-row border-b border-b-[#9199E1] py-2">
+            <img
+              src="/Logo Shinden.png"
+              className="h-16 justify-self-center"
+              alt="Logo ARgg"
+            />
+          </div>
+          <div className="grid grid-flow-row border-b border-b-[#9199E1] py-2">
+            <img
+              src="/Logo Shinden.png"
+              className="h-16 justify-self-center"
+              alt="Logo ARgg"
+            />
+          </div>
+          <div className="grid grid-flow-row py-2">
+            <img
+              src="/Logo Shinden.png"
+              className="h-16 justify-self-center"
+              alt="Logo ARgg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
---
+++ src/app/components/carousel.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Carousel: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = ["/AGS.png", "/AGS.png", "/AGS.png"];
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div className="relative mx-32 mb-14">
+      <div className="relative h-56 overflow-hidden md:h-96">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`absolute inset-0 transition-opacity duration-700 ease-in-out grid justify-items-center content-center rounded-lg${
+              index === currentIndex ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <img src={image} className="block" alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-between p-4">
+        <button
+          onClick={prevSlide}
+          className="bg-white/30 dark:bg-gray-800/30 hover:bg-white/50 dark:hover:bg-gray-800/60 focus:ring-4 focus:ring-white dark:focus:ring-gray-800/70 rounded-full p-2"
+        >
+          <span className="sr-only">Previous</span>❮
+        </button>
+        <button
+          onClick={nextSlide}
+          className="bg-white/30 dark:bg-gray-800/30 hover:bg-white/50 dark:hover:bg-gray-800/60 focus:ring-4 focus:ring-white dark:focus:ring-gray-800/70 rounded-full p-2"
+        >
+          <span className="sr-only">Next</span>❯
+        </button>
+      </div>
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-3">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`w-3 h-3 rounded-full ${
+              index === currentIndex ? "bg-white" : "bg-white/50"
+            }`}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
