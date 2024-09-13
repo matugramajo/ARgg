d754d91b43afaebfda1a20ad507aa70eaab2f72b
Matilde Gramajo <matildegramajo@hotmail.com>
2024-09-13 13:34:15 -0300

    fix scrollbar horizontal

--- src/app/components/menujuegos.tsx
+++ src/app/components/menujuegos.tsx
@@ -17,46 +17,50 @@ import {
 const MenuJuegos: React.FC = () => {
   return (
     <div className="overflow-auto">
-      <div className="flex justify-center md:my-14 md:pb-5 overflow-x-auto custom-scrollbar mt-4">
-        <div className="flex flex-row bg-[#232240] p-3 rounded-lg">
+      <div
+        className="flex justify-start md:my-14 md:pb-5 pb-3 overflow-x-auto custom-scrollbar scroll-smooth"
+        style={{ scrollSnapType: "x mandatory" }}
+      >
+        <div
+          className="flex flex-row bg-[#232240] p-3 rounded-lg"
+          style={{ scrollSnapAlign: "start" }}
+        >
+          {" "}
           <div className="px-2">
             <Joystick />
           </div>
-          <div className="px-2">
-            <a>Todos los juegos</a>
-          </div>
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <LoL />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <CS />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <Valo />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <Dota />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <Fifa />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <R6 />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <Rocket />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <PUBG />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <MobileLegends />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <Fortnite />
         </div>
-        <div className="px-10">
+        <div className="px-10" style={{ scrollSnapAlign: "start" }}>
           <Sim />
         </div>
       </div>
