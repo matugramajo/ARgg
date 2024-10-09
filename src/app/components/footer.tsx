c7277291f7fb90f80781e862ab02c383374f5b41
Matilde Gramajo <matildegramajo@hotmail.com>
2024-10-09 01:07:48 -0300

    cambio footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,14 +4,11 @@ import { X, Github, Linkedin } from "./logos";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#131313]">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 md:mt-14 mt-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="/"
-            className="flex md:mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <div className="relative h-14 w-28">
+    <footer className="bg-[#131313cc] text-gray-400 md:py-8 py-5 md:mt-20 mt-10">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className=" flex md:flex-col flex-row items-center md:items-start mb-4 md:mb-0">
+            <div className="h-8 w-24 md:h-14 md:w-28">
               <Image
                 src="/Logo-ARgg-2.png"
                 layout="responsive"
@@ -21,43 +18,48 @@ const Footer: React.FC = () => {
                 alt="ARgg Logo"
               />
             </div>
-          </a>
-        </div>
-        <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
-        <div className="flex flex-col md:flex-row justify-between">
-          <div>
-            <span className="block text-sm text-gray-400">
+            <div className="hidden md:flex text-sm pl-3 md:pl-0">
               Diseñado y desarrollado por Matilde Gramajo.
-            </span>
+            </div>
           </div>
-          <div className="flex pt-4 md:pt-0">
-            <div className="px-2">
-              <a
-                href="https://x.com/_____matilde"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <X />
-              </a>
-            </div>
-            <div className="px-2">
-              <a
-                href="https://github.com/matugramajo"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Github />
-              </a>
-            </div>
-            <div className="px-2">
-              <a
-                href="https://www.linkedin.com/in/matildegramajo/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Linkedin />
-              </a>
-            </div>
+          <div className="flex space-x-4 mb-4 md:mb-0">
+            <a
+              href="https://x.com/_____matilde"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-6 h-6"
+            >
+              <X />
+            </a>
+            <a
+              href="https://github.com/matugramajo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-6 h-6"
+            >
+              <Github />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/matildegramajo/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-6 h-6"
+            >
+              <Linkedin />
+            </a>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-xs mb-3">
+              Si te resultó util y te gustó mi trabajo
+            </p>
+            <a
+              href="https://cafecito.app/matugramajo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xs bg-[#423f76cc] p-2 rounded-xl overflow-hidden transform transition-all duration-300 hover:scale-105 hover:text-white"
+            >
+              Invitame un cafecito (:
+            </a>
           </div>
         </div>
       </div>
